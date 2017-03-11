a3e9c66308778b2e61c02f8e72cf794f93cf1561
Everesty <Everesty@users.noreply.github.com>
2017-03-10 21:06:04 -0700

    :sparkles: :snowflake: Finish the minimum site

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # Northridge TSA Website
 
-This repository holds the source for a website created to compete in the [TSA](http://tsaweb.org/)
-[Webmaster Competition](http://tsaweb.org/High-School-Competitions).
+This repository holds the source for a website created to compete in the
+[TSA](http://tsaweb.org/) [Webmaster Competition](http://tsaweb.org/High-School-Competitions).
+Utilizing popular web frameworks and tools, such as webpack, React, and Sass,
+this repository contains it all.
+
+To build/develop, there are a few npm scripts to help you:
+
+* `npm start` starts the `webpack-dev-server`, hot reloading included!
+* `npm run build` builds the application into the `dist/` directory.
+* `npm run dev-production` builds the application in the production environment,
+  but still runs the `webpack-dev-server`.
+
+---
+
+All credits can be found in their own document: [CREDITS.md](/CREDITS.md).
+This same document is rendered on the site under the url path `/credits`
--- app/components/ARPage/ARPage.jsx
+++ app/components/ARPage/ARPage.jsx
@@ -2,24 +2,37 @@ import React from 'react';
 
 import './ARPage.scss';
 
-export default class ARPage extends React.Component {
-  componentDidMount() {
-    document.body.classList.add('augmented-reality');
-  }
-
-  componentWillUnmount() {
-    document.body.classList.remove('augmented-reality');
-  }
-
-  render() {
-    return (
-      <div id="ARPage" />
-    );
-  }
-}
-
-/*
 export default () => (
-  <div id="ARPage" />
+  <div id="ARPage">
+    <section className="content">
+      <h1>Augmented Reality</h1>
+      <p>
+        Augmented Reality is becoming a reality more and more. As it stands, AR
+        is already integrating itself into more and more modern technology:
+        games like Pokemon Go, utilities like Google Translate, and entertainment
+        like Snapchat Filters.
+      </p>
+      <p>
+        Future uses of AR include interactive chatting with friends, shopping
+        where you can try on clothing/furnature in your own home, and even
+        personal assistants.
+      </p>
+      <p>
+        The benefits of Augmented Reality are clear just by its name:
+        <i>Augmented Reality;</i> making reality better. As such, AR tends to
+        increase the quality of life.
+      </p>
+      <p>
+        Most AR technology can be shared with Virtual Reality. AR-specific
+        technology, however, mostly comprises of Computer Vision and rendering.
+      </p>
+      <p>
+        One of the biggest concerns about AR technology is straight-up the
+        security of AR technology; malicious breaches of security can lead to
+        all kinds of problems, like &quot;perceiving&quot; an obejct that
+        doesn&apos;t exist, or leaking sensor data (camera, microphone, etc.)
+        to a remote host.
+      </p>
+    </section>
+  </div>
 );
-*/
